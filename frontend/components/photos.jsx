8e5ee83f3c3e3d8905ca82a6c1e4d0fa6c919036
Will Johnson <will.p.johnson@gmail.com>
2018-07-23 14:02:58 -0400

    Rearranged

--- frontend/components/photos.jsx
+++ frontend/components/photos.jsx
@@ -12,14 +12,6 @@ class Photos extends React.Component {
     return(
       <div className="photos">
         <div className="photo-row">
-          <Photo classes="photo size-4" source="assets/images/food_shots/funnel.jpg" />
-          <div className="photo-stack">
-            <Photo classes="photo stack-size-1" source="assets/images/food_shots/lasagna.jpg" />
-            <Photo classes="photo stack-size-1" source="assets/images/food_shots/sword2.jpg" />
-          </div>
-          <Photo classes="photo size-4" source="assets/images/food_shots/brisket.jpg" />
-        </div>
-        <div className="photo-row">
           <Photo classes="photo size-2" source="assets/images/food_shots/salmon.jpg" />
           <Photo classes="photo size-2" source="assets/images/food_shots/pancakes.jpg" />
         </div>
@@ -29,22 +21,34 @@ class Photos extends React.Component {
           <Photo classes="photo size-3" source="assets/images/food_shots/spagnmeat.jpg" />
         </div>
         <div className="photo-row">
+          <Photo classes="photo size-2" source="assets/images/food_shots/lasagna.jpg" />
+          <Photo classes="photo size-2" source="assets/images/food_shots/sword2.jpg" />
+        </div>
+        <div className="photo-row">
           <Photo classes="photo size-3" source="assets/images/food_shots/shrimpalf.jpg" />
           <Photo classes="photo size-3" source="assets/images/food_shots/wonton.jpg" />
           <Photo classes="photo size-3" source="assets/images/food_shots/adobo.jpg" />
+        </div>
+        <div className="photo-row">
+          <Photo classes="photo size-2" source="assets/images/food_shots/tenderloin.jpg" />
+          <Photo classes="photo size-2" source="assets/images/food_shots/cornbread.jpg" />
         </div>
         <div className="photo-row">
           <Photo classes="photo size-3" source="assets/images/food_shots/fried_chicken.jpg" />
           <Photo classes="photo size-3" source="assets/images/food_shots/shepherd.jpg" />
           <Photo classes="photo size-3" source="assets/images/food_shots/loin.jpg" />
         </div>
-        <div className="photo-row">
-          <Photo classes="photo size-2" source="assets/images/food_shots/tenderloin.jpg" />
-          <Photo classes="photo size-2" source="assets/images/food_shots/cornbread.jpg" />
-        </div>
       </div>
     )
   }
 }
 
+// <div className="photo-row">
+//   <Photo classes="photo size-4" source="assets/images/food_shots/funnel.jpg" />
+//   <div className="photo-stack">
+//     <Photo classes="photo stack-size-1" source="assets/images/food_shots/lasagna.jpg" />
+//     <Photo classes="photo stack-size-1" source="assets/images/food_shots/sword2.jpg" />
+//   </div>
+//   <Photo classes="photo size-4" source="assets/images/food_shots/brisket.jpg" />
+// </div>
 export default withRouter(Photos);
